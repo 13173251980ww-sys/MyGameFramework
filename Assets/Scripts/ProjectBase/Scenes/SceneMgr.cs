c933e1e16f78dfb42848128f0a8a29531d7284ae
ChenShushuo <13173251980ww@gmail.com>
2026-07-24 20:08:20 +0800

    feat: 场景切换模块_01

---
+++ Assets/Scripts/ProjectBase/Scenes/SceneMgr.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneMgr : BaseMgr<SceneMgr>
+{
+    /// <summary>
+    /// 同步加载场景
+    /// </summary>
+    /// <param name="name"></param>
+    public void Load(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    
+    /// <summary>
+    /// 异步加载场景
+    /// </summary>
+    /// <param name="name"></param>
+    public void LoadAsync(string name,UnityAction callback =null)
+    {
+        MonoMgr.instance.StartCoroutine(ReallyLoadSAsync(name,callback));
+    }
+
+    IEnumerator ReallyLoadSAsync(string name,UnityAction callback =null)
+    {
+        AsyncOperation ao= SceneManager.LoadSceneAsync(name);
+        while (!ao.isDone)
+        {
+            EventMgr.instance.EventTrigger("进度条更新", ao.progress);
+            yield return ao;
+        }
+        
+        callback?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Test/Scenes/TestScene.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class TestScene : MonoBehaviour
+{
+    void Start()
+    {
+        EventMgr.instance.AddListener("进度条更新", (obj) =>
+        {
+            Debug.Log("进度条更新中，进度为:" + obj);
+        });
+        
+        SceneMgr.instance.LoadAsync("Scene2", () =>
+        {
+            Debug.Log("异步加载，切换到场景2");
+        });
+    }
+}
