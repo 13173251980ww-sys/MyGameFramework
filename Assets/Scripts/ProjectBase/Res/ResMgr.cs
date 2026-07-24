83252cf414a3f0420051b4667ffeb46bd819b721
ChenShushuo <13173251980ww@gmail.com>
2026-07-24 19:51:52 +0800

    feat: 资源加载模块_01

---
+++ Assets/Scripts/ProjectBase/Res/ResMgr.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ResMgr : BaseMgr<ResMgr>
+{
+    /// <summary>
+    /// 同步加载资源
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public T Load<T>(string name)  where T : Object
+    {
+        T res = Resources.Load<T>(name);
+        if (res is GameObject)
+        {
+            return GameObject.Instantiate(res);
+        }
+
+        return res;
+    }
+
+    
+    /// <summary>
+    /// 异步加载资源
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public void LoadAsync<T>(string name ,UnityAction<T> callback =null) where T : Object
+    {
+        MonoMgr.instance.StartCoroutine(ReallyLoadAsync<T>(name,callback));
+    }
+
+    IEnumerator ReallyLoadAsync<T>(string name,UnityAction<T> callback =null) where T : Object
+    {
+        ResourceRequest r=Resources.LoadAsync(name);
+        yield return r;
+
+        if (r.asset is GameObject)
+        {
+            callback?.Invoke(GameObject.Instantiate(r.asset) as T);
+        }
+        else
+        {
+            callback?.Invoke(r.asset as T);
+        }
+    }
+}
---
+++ Assets/Scripts/Test/Res/TestRes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TestRes: MonoBehaviour
+{
+    void Start()
+    {
+        ResMgr.instance.LoadAsync<GameObject>("A", (obj) =>
+        {
+            Debug.Log(obj.name);
+        });
+    }
+}
