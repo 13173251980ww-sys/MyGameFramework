951b5a66983825ccafca408f974aaccfe5c9d76e
ChenShushuo <13173251980ww@gmail.com>
2026-07-24 20:13:44 +0800

    feat: 输入模块_01

---
+++ Assets/Scripts/ProjectBase/Input/InputMgr.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class InputMgr : BaseMgr<InputMgr>
+{ 
+    private bool _isStart = false;
+
+    public void Start()
+    {
+        _isStart = true;
+    }
+
+    public void Stop()
+    {
+        _isStart = false;
+    }
+    
+    public InputMgr()
+    {
+        MonoMgr.instance.AddEventListener(MyUpdate);
+    }
+
+    void MyUpdate()
+    {
+        if (!_isStart)
+            return;
+        
+        CheckInput(KeyCode.W);
+        CheckInput(KeyCode.A);
+        CheckInput(KeyCode.S);
+        CheckInput(KeyCode.D);
+    }
+
+    public void CheckInput(KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+            EventMgr.instance.EventTrigger("某键按下",key);
+        
+        if (Input.GetKeyUp(key))
+            EventMgr.instance.EventTrigger("某键抬起",key);
+    }
+}
---
+++ Assets/Scripts/Test/Input/TestInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TestInput : MonoBehaviour
+{
+    void Start()
+    {
+        InputMgr.instance.Start();
+        EventMgr.instance.AddListener("某键抬起", (key) =>
+        {
+            switch (key)
+            {
+                case KeyCode.W:
+                    Debug.Log("向前走");
+                    break;
+                case KeyCode.A:
+                    Debug.Log("向左走");
+                    break;
+                case KeyCode.S:
+                    Debug.Log("向下走");
+                    break;
+                case KeyCode.D:
+                    Debug.Log("向右走");
+                    break;
+            }
+        });
+    }
+}
